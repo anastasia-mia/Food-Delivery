10749e9ad725b8c00f2762aeab9c5e3b58b4d8f7
anastasia_mia <nastia.mianovska333@gmail.com>
2024-08-02 12:21:41 +0300

    implemented persisted cart

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import cartReducer from './cartSlice'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: persistedReducer,
     },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
