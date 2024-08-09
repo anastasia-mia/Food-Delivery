4bf0be9a05bbda5bf3d18601f049262508f8150a
anastasia_mia <nastia.mianovska333@gmail.com>
2024-08-09 17:51:00 +0300

    added random restaurant location in view of client location

--- client/src/pages/CheckoutPage/Map/Map.tsx
+++ client/src/pages/CheckoutPage/Map/Map.tsx
@@ -8,6 +8,19 @@ const MapComponent = () => {
     const mapRef = useRef<HTMLDivElement | null>(null);
     const mapInstance = useRef<L.Map | null>(null);
 
+    const getRandomCoordinates = (centerLat: number, centerLng: number, minRadius: number, maxRadius: number) => {
+        const radius = Math.random() * (maxRadius - minRadius) + minRadius;
+        const randomAngle = Math.random() * 2 * Math.PI;
+
+        const offsetLat = radius * Math.cos(randomAngle) / 111.32;
+        const offsetLng = radius * Math.sin(randomAngle) / (111.32 * Math.cos(centerLat * Math.PI / 180));
+
+        return {
+            lat: centerLat + offsetLat,
+            lng: centerLng + offsetLng,
+        };
+    };
+
     useEffect(() => {
         if (mapInstance.current) return;
 
@@ -27,6 +40,26 @@ const MapComponent = () => {
                         L.marker([latitude, longitude]).addTo(mapInstance.current)
                             .bindPopup('Your Location')
                             .openPopup();
+
+                        const randomCoords = getRandomCoordinates(latitude, longitude, 1, 4);
+                        L.marker([randomCoords.lat, randomCoords.lng]).addTo(mapInstance.current)
+                            .bindPopup('Restaurant')
+                            .openPopup();
+
+                        if (mapInstance.current) {
+                            L.Routing.control({
+                                waypoints: [
+                                    L.latLng(latitude, longitude),
+                                    L.latLng(randomCoords.lat, randomCoords.lng)
+                                ],
+                                routeWhileDragging: true,
+                                showAlternatives: false
+                            }).addTo(mapInstance.current);
+                            const element = document.querySelector('.leaflet-routing-container');
+                            if (element) {
+                                (element as HTMLElement).style.display = 'none';
+                            }
+                        }
                     }
                 },
             );
